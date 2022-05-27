db9ae17179b5b7e0e7afd77244603b7754a1f1a5
shubham-ch247 <shubham.s@chapter247.com>
2022-05-27 15:06:26 +0530

    change catch block and subject messaage

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const userModelSchema = require("../model/user");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { Email } = require("../utils/Email");
 //signup user
@@ -9,61 +9,68 @@ const signup = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await userModelSchema.findOne({ email: email });
     if (user) {
-      throw new Error("email already exist", 422);
+      return res.status(404).json({
+        message: "email already exist!",
+      });
     }
-    const newUser = await userModelSchema.create({
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: hashedPassword,
-    });
+    const newUser = await userModelSchema.create({firstName,lastName,email,password: hashedPassword,});
     return res.status(200).json({
       registerUser: newUser,
     });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({
-      error: "user already exist!",
+      success: false,
+      message:
+        "we are having some error while completing your request. Please try again after some time.",
+      error: error,
     });
   }
 };
-
-// forgot password page action
+// forgot password 
 const forgotPassword = async (req, res, next) => {
   try {
     const { email } = req.body;
     const user = await userModelSchema.findOne({ email: email });
+    if (!user) {
+      return res.status(404).json({
+        message: "email not found!",
+      });
+    }
     // set the secret with jwt token + password
     const secret = process.env.jwt_secret + user.password;
-    const payLoad = {
+    const payload = {
       email: user.email,
       id: user._id,
     };
-    const token = jwt.sign(payLoad, secret, { expiresIn: '60m' });
+    const token = jwt.sign(payload, secret, { expiresIn: "60m" });
     const link = `http://localhost:8080/user/newpassword/${user._id}/${token}`;
     // send email to set the new password
     const emailClient = new Email();
     emailClient.setBody(link);
-    emailClient.setSubject("verify your email");
+    emailClient.setSubject("token verification!");
     emailClient.send(email);
     console.log(email);
-
     return res.status(200).json({
       link: link,
     });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({
-      error: "email not found!",
+      success: false,
+      message:
+        "we are having some error while completing your request. Please try again after some time.",
+      error: error,
     });
   }
 };
-
+//verify link
 const verifyLink = async (req, res, next) => {
   const { id, token } = req.params;
-  const user = await userModelSchema.findOne({ _id: id});
+  const user = await userModelSchema.findOne({ _id: id });
   const secret = process.env.jwt_secret + user.password;
   try {
-    const payLoad = jwt.verify(token, secret);
-
+    const payload = jwt.verify(token, secret);
     return res.status(200).json({
       verify: "link verification successfully",
     });
@@ -73,7 +80,7 @@ const verifyLink = async (req, res, next) => {
     });
   }
 };
-
+//create new password
 const newPassword = async (req, res, next) => {
   const { id, token } = req.params;
   const { password, cpassword } = req.body;
@@ -81,9 +88,9 @@ const newPassword = async (req, res, next) => {
   const user = await userModelSchema.findOne({ _id: id });
   const secret = process.env.jwt_secret + user.password;
   try {
-    const payLoad = jwt.verify(token, secret);
+    const payload = jwt.verify(token, secret);
     // validate password and confirm password should match
-    // we can simply find the user with the payLoad email and id and finally update with new password
+    // we can simply find the user with the payload email and id and finally update with new password
     if (password == cpassword) {
       await userModelSchema.findOneAndUpdate(
         { _id: id },
--- routes/user/auth.js
+++ routes/user/auth.js
@@ -6,11 +6,9 @@ const {
   verifyLink,
 } = require("../../controllers/auth");
 const authRouter = express.Router();
-
 authRouter.post("/signup", signup);
 authRouter.post("/forgotPassword", forgotPassword);
 // new password
 authRouter.get("/newpassword/:id/:token", verifyLink);
 authRouter.post("/newpassword/:id/:token", newPassword);
-
 module.exports = authRouter;
